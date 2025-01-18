ec540affc2488e5109299f29c9eae44e40c436e7
olrlozl <thakd221@naver.com>
2025-01-19 00:40:26 +0900

    feat: User 모델에 kakaoId, socketId, friendRequests 필드 추가 및 스키마 수정

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -4,22 +4,53 @@ type UserStateType = 'online' | 'offline' | 'playing';
 
 interface IUser extends Document {
   nickname: string;
+  kakaoId: number;
+  socketId: String;
   wins: number;
   losses: number;
-  state: UserStateType;
   rank: number;
+  state: UserStateType;
   friends: mongoose.Types.ObjectId[];
+  friendRequests: {
+    sent: mongoose.Types.ObjectId[];
+    received: mongoose.Types.ObjectId[];
+  };
 }
 
 const userSchema = new Schema<IUser>({
-  nickname: { type: String, required: true, unique: true, minlength: 2, maxlength: 6 },
+  nickname: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 2,
+    maxlength: 6,
+  },
+  kakaoId: { type: Number, required: true, unique: true },
+  socketId: { type: Number, unique: true },
   wins: { type: Number, required: true, default: 0 },
   losses: { type: Number, required: true, default: 0 },
+  rank: { type: Number, default: null },
   state: { type: String, required: true, default: 'online' },
-  rank: { type: Number, required: true, default: null },
-  friends: [ { type: mongoose.Types.ObjectId, ref: 'User', default: []}]
-})
-
+  friends: {
+    type: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    default: [],
+  },
+  friendRequests: {
+    sent: {
+      type: [{ type: mongoose.Types.ObjectId, ref: 'User' }],
+      default: [],
+    },
+    received: {
+      type: [{ type: mongoose.Types.ObjectId, ref: 'User' }],
+      default: [],
+    },
+  },
+});
 
 const User = mongoose.model<IUser>('User', userSchema);
 export default User;
