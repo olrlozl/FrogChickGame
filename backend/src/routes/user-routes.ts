23754de8cf8389a6ee60726d532a76a8847e1397
olrlozl <thakd221@naver.com>
2025-01-22 02:27:53 +0900

    feat: 로그아웃 시 JWT 토큰 검증 미들웨어 추가

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -4,11 +4,12 @@ import {
   kakaoLogin,
   kakaoLogout,
 } from '../controllers/user-controllers';
+import { checkAuth } from '../middleware/auth-middleware';
 
 const userRouter = Router();
 
 userRouter.post('/nickname', createUser);
 userRouter.post('/login/kakao', kakaoLogin);
-userRouter.post('/logout/kakao', kakaoLogout);
+userRouter.post('/logout/kakao', checkAuth, kakaoLogout); // 로그아웃 시 JWT 토큰 검증
 
 export default userRouter;
