608886ea25f249a167904c9f76d6fba7d34a89d6
olrlozl <thakd221@naver.com>
2025-01-24 01:32:30 +0900

    refactor: user-service 함수에 HttpError 에러 타입 추가

--- backend/src/services/user-service.ts
+++ backend/src/services/user-service.ts
@@ -6,9 +6,9 @@ import redisClient from '../config/redis-client';
 dotenv.config();
 const KAKAO_REST_API_KEY = process.env.KAKAO_REST_API_KEY;
 
+// 인가 코드로 카카오 액세스 토큰과 카카오 리프레시 토큰 받기
 const getKakaoTokens = async (redirectUri: string, code: string) => {
   try {
-    // 인가 코드로 토큰 받기
     const response = await axios.post(
       'https://kauth.kakao.com/oauth/token',
       null,
@@ -25,18 +25,20 @@ const getKakaoTokens = async (redirectUri: string, code: string) => {
       }
     );
 
-    // 응답에서 액세스 토큰과 리프레시 토큰 받기
     const { access_token, refresh_token } = response.data;
-
     return { kakaoAccessToken: access_token, kakaoRefreshToken: refresh_token };
   } catch (error) {
-    throw new HttpError('카카오 토큰 가져오기에 실패했습니다.', 500);
+    throw new HttpError(
+      '카카오 토큰 가져오기에 실패했습니다.',
+      500,
+      'FAILED_GET_KAKAO_TOKEN'
+    );
   }
 };
 
+// 카카오 엑세스 토큰으로 kakaoId 조회
 const getUserKakaoId = async (kakaoAccessToken: string) => {
   try {
-    // 엑세스 토큰으로 사용자 정보 가져오기
     const response = await axios.get('https://kapi.kakao.com/v2/user/me', {
       headers: {
         Authorization: `Bearer ${kakaoAccessToken}`,
@@ -46,13 +48,17 @@ const getUserKakaoId = async (kakaoAccessToken: string) => {
     const kakaoId: number = response.data.id;
     return kakaoId;
   } catch (error) {
-    throw new HttpError('카카오 사용자 정보 가져오기에 실패했습니다.', 500);
+    throw new HttpError(
+      '카카오 사용자 정보 가져오기에 실패했습니다.',
+      500,
+      'FAILED_GET_KAKAOID'
+    );
   }
 };
 
+// 카카오 엑세스 토큰으로 카카오 로그아웃
 const logoutKakao = async (kakaoAccessToken: string) => {
   try {
-    // 엑세스 토큰으로 로그아웃
     const response = await axios.post(
       'https://kapi.kakao.com/v1/user/logout',
       null,
@@ -64,10 +70,15 @@ const logoutKakao = async (kakaoAccessToken: string) => {
     );
     return response.data;
   } catch (error) {
-    throw new HttpError('카카오 로그아웃에 실패했습니다.', 500);
+    throw new HttpError(
+      '카카오 로그아웃에 실패했습니다.',
+      500,
+      'FAILED_KAKAO_LOGOUT'
+    );
   }
 };
 
+// Redis에 카카오 엑세스 토큰 저장
 const storeKakaoAccessTokenInRedis = async (
   userId: string,
   kakaoAccessToken: string
@@ -78,32 +89,44 @@ const storeKakaoAccessTokenInRedis = async (
     });
   } catch (error) {
     throw new HttpError(
-      'Redis에 카카오 액세스 토큰 저장하기에 실패했습니다.',
-      500
+      'Redis에 카카오 액세스 토큰 저장에 실패했습니다.',
+      500,
+      'FAILED_STORE_KAKAO_ACCESS_TOKEN'
     );
   }
 };
 
+// Redis에서 카카오 엑세스 토큰 조회
 const getKakaoAccessTokenFromRedis = async (userId: string) => {
   try {
     const token = await redisClient.get(`kakaoAccessToken:${userId}`);
     if (!token) {
-      throw new HttpError('해당 사용자의 액세스 토큰을 찾을 수 없습니다.', 404);
+      throw new HttpError(
+        '해당 사용자의 카카오 액세스 토큰을 찾을 수 없습니다.',
+        404,
+        'NOT_FOUND_KAKAO_ACCESS_TOKEN'
+      );
     }
     return token;
   } catch (error) {
     throw new HttpError(
-      'Redis에서 카카오 액세스 토큰 조회하기에 실패했습니다.',
-      500
+      'Redis에서 카카오 액세스 토큰 조회에 실패했습니다.',
+      500,
+      'FAILED_GET_KAKAO_ACCESS_TOKEN'
     );
   }
 };
 
+// Redis에서 카카오 엑세스 토큰 제거
 const removeKakaoAccessTokenFromRedis = async (userId: string) => {
   try {
     await redisClient.del(`kakaoAccessToken:${userId}`);
   } catch (error) {
-    throw new HttpError('Redis에서 토큰 제거에 실패했습니다.', 500);
+    throw new HttpError(
+      'Redis에서 카카오 엑세스 토큰 제거에 실패했습니다.',
+      500,
+      'FAILED_REMOVE_KAKAO_ACCESS_TOKEN'
+    );
   }
 };
 
