7101f8b484084af7aa9ab80b1662d10c9feee03e
olrlozl <thakd221@naver.com>
2025-01-24 01:09:14 +0900

    feat: jwt 리프레시 토큰 발급 함수 추가

--- backend/src/utils/jwt-util.ts
+++ backend/src/utils/jwt-util.ts
@@ -21,4 +21,20 @@ const generateJwtAccessToken = (payload: JwtPayload) => {
   }
 };
 
-export { generateJwtAccessToken };
+const generateJwtRefreshToken = (payload: JwtPayload) => {
+  try {
+    // jwt 리프레시 토큰 발급 (만료시간: 7일)
+    const jwtRefreshToken = jwt.sign(payload, jwtSecretKey, {
+      expiresIn: '7d',
+    });
+    return jwtRefreshToken;
+  } catch (error) {
+    throw new HttpError(
+      'jwt 리프레시 토큰 생성에 실패했습니다.',
+      500,
+      'FAILED_GENERATION_JWT_REFRESH_TOKEN'
+    );
+  }
+};
+
+export { generateJwtAccessToken, generateJwtRefreshToken };
