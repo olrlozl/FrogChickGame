e89537d36f3792b63348ac7ca31809887f09faaf
JangHyunsu <wkd940709@naver.com>
2025-01-31 00:52:40 +0900

    feat: 사용자 입력에 따른 닉네임 변화

--- frontend/src/components/user/NicknameInput.tsx
+++ frontend/src/components/user/NicknameInput.tsx
@@ -1,18 +1,36 @@
 import 'styles/components/user/nickname-input.scss';
+import { ChangeEvent, SetState } from 'types/common';
+
+const NICKNAME_MIN_LENGTH = 2;
+const NICKNAME_MAX_LENGTH = 6;
 
 interface NicknameInputProps {
   text: string;
+  nickname: string;
+  setNickname: SetState<string>;
+  setErrorMessage: SetState<string>
 }
 
-const NicknameInput = ({ text }: NicknameInputProps) => {
+const NicknameInput = ({ text, nickname, setNickname, setErrorMessage }: NicknameInputProps) => {
+  const handleChangeNickname = (e: ChangeEvent) => {
+    setErrorMessage('');
+    if (e.target.value.length > NICKNAME_MAX_LENGTH) {
+      setNickname(e.target.value.slice(0, NICKNAME_MAX_LENGTH));
+    } else {
+      setNickname(e.target.value)
+    }
+  }
+
   return (
     <input
       className='nickname-input'
       type="text"
       placeholder={text}
+      value={nickname}
+      onChange={(e) => handleChangeNickname(e)}
       pattern="^[가-힣a-zA-Z]{2,6}$"
-      minLength={2}
-      maxLength={6}
+      minLength={NICKNAME_MIN_LENGTH}
+      maxLength={NICKNAME_MAX_LENGTH}
     />
   );
 };
