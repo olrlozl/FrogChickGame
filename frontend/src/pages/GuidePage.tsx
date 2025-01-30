f9f419dcd63d042cdc82e5c7d218b91ca386d72b
JangHyunsu <wkd940709@naver.com>
2025-01-31 00:50:29 +0900

    feat: 로그아웃 로직 및 로그아웃 확인 모달

--- frontend/src/pages/GuidePage.tsx
+++ frontend/src/pages/GuidePage.tsx
@@ -1,26 +1,54 @@
-import Logout from 'components/account/Logout';
 import GuideBalloon from 'components/guide/GuideBalloon';
 import 'styles/pages/guide-page.scss';
 import farmer from 'assets/images/farmer.png';
 import pond from 'assets/images/pond.png';
 import { useState } from 'react';
-import { GuideOptionType } from 'types/guide';
+import KakaoButton from 'components/user/KakaoButton';
+import Modal from 'components/common/Modal/Modal';
+import { modalProps } from 'constants/modal';
+import { useModal } from 'hooks/common/useModal';
+import { GuideOption } from 'types/guide';
+import { useLogout } from 'hooks/user/useLogout';
 
 const GuidePage = () => {
-  const [selectedOption, setSelectedOption] = useState<GuideOptionType>('rule');
+  const [selectedOption, setSelectedOption] = useState<GuideOption>('rule');
+  const { isModalOpen, openModal, closeModal } = useModal();
+  const { message, messageFontSize, btns } = modalProps.logoutConfirm;
 
-  let guideImage = selectedOption === 'rule' ? pond : farmer;
+  const guideImage = selectedOption === 'rule' ? pond : farmer;
 
-  const handleClickChangeOption = (nextOption: GuideOptionType) => {
+  const handleClickChangeOption = (nextOption: GuideOption) => {
     if (nextOption !== selectedOption)
       setSelectedOption((prev) => (prev === 'rule' ? 'control' : 'rule'));
   };
 
+  const executeKakaoLogout = useLogout(closeModal);
+
   return (
     <div className="guide-page">
-      <GuideBalloon guideOption={selectedOption} onClick={handleClickChangeOption} />
-      <img src={guideImage} alt='구리와 농부' />
-      <Logout />
+      <GuideBalloon
+        guideOption={selectedOption}
+        onClick={handleClickChangeOption}
+      />
+      <img src={guideImage} alt="구리와 농부" />
+      <KakaoButton onClick={openModal} kakaoOption="로그아웃" />
+      <Modal
+        isOpen={isModalOpen}
+        message={message}
+        messageFontSize={messageFontSize}
+        btns={[
+          {
+            label: btns[0].label,
+            onClick: executeKakaoLogout,
+            type: btns[0].type,
+          },
+          {
+            label: btns[1].label,
+            onClick: closeModal,
+            type: btns[1].type,
+          },
+        ]}
+      />
     </div>
   );
 };
