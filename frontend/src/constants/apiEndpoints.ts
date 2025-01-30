1226d527a4dbb56b925d3f471ef032362c76a2aa
JangHyunsu <wkd940709@naver.com>
2025-01-31 00:42:17 +0900

    feat: api url, 에러메세지, modal props, 쿼리 키 상수화

---
+++ frontend/src/constants/apiEndpoints.ts
@@ -0,0 +1,13 @@
+export const API_ENDPOINTS = {
+  KAKAO_LOGIN: '/user/login/kakao',
+  KAKAO_LOGOUT: '/user/logout/kakao',
+  CREATE_USER: '/user/nickname',
+  REFRESH_JWT_ACCESS_TOKEN: '/user/refresh/jwt-access-token',
+  SEARCH_FRIEND: '/friend/search',
+  GET_FRIENDS: '/friend',
+  APPLY_FRIEND: '/friend/apply',
+  GET_FRIEND_RECEIPTS: '/friend/receipts',
+  RECEPT_FRIEND: '/friend/recept',
+  CANCEL_FRIEND_APPLY: (nickname: string) => `friend/${nickname}`,
+  GET_RANK: '/rank'
+}
---
+++ frontend/src/constants/queryKeys.ts
@@ -0,0 +1,10 @@
+// 문자열을 직접 입력할 때 오타가 있을 수 있기 때문에 
+export const QUERY_KEYS = {
+  friends: 'friends',
+  ranks: 'ranks'
+}
+
+export const MUTATIONS_KEYS = {
+  login: 'login',
+  logout: 'logout',
+}
