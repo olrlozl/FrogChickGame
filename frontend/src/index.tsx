2928b64f2d2cd9ee1020ebe6926a715dcbfd0078
JangHyunsu <wkd940709@naver.com>
2025-02-03 19:25:06 +0900

    refactor: 홑따옴표로 변경

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import './styles/main.scss';
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   // <React.StrictMode>
-    <App />
+  <App />
   // </React.StrictMode>
 );
 
