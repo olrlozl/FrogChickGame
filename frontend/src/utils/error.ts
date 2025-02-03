7fab65a6e3b582636f7e624e71b99da18ed19b13
JangHyunsu <wkd940709@naver.com>
2025-02-03 19:30:57 +0900

    fix: 에러 필터링 조건 추가

--- frontend/src/utils/error.ts
+++ frontend/src/utils/error.ts
@@ -8,6 +8,11 @@ export const errorHandle = (
   errorKey: string
 ) => {
   if (e instanceof AxiosError && e.response?.data.errorType) {
-    setErrorMessage(ERROR_MESSAGES[errorKey][e.response.data.errorType]);
+    // axiosInstance에서 처리한 에러를 중복 처리하지 않기 위해
+    if (
+      e.response.data.errorType !== 'EXPIRED_JWT_TOKEN' &&
+      e.response.data.errorType !== 'MISSING_JWT_ACCESS_TOKEN'
+    )
+      setErrorMessage(ERROR_MESSAGES[errorKey][e.response.data.errorType]);
   }
 };
