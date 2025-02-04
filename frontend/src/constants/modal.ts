be432ea846718c6d86236433ed9cc7112c408390
JangHyunsu <wkd940709@naver.com>
2025-02-05 01:26:15 +0900

    refactor: gameResult 요소 추가 및 ModalProps 구조 수정

--- frontend/src/constants/modal.ts
+++ frontend/src/constants/modal.ts
@@ -2,11 +2,11 @@ import { ButtonType, MessageFontSize } from 'types/common';
 
 interface ModalProps {
   message: string;
-  messageFontSize: MessageFontSize;
+  messageFontSize?: MessageFontSize;
   btns: { label: string; type: ButtonType }[];
 }
 
-type ModalKeys = 'logoutConfirm' | 'createNickname' | 'error';
+type ModalKeys = 'logoutConfirm' | 'createNickname' | 'gameResult' | 'error';
 
 type ModalPropsType = {
   [K in ModalKeys]: ModalProps;
@@ -14,8 +14,7 @@ type ModalPropsType = {
 
 export const modalProps: ModalPropsType = {
   logoutConfirm: {
-    message: '정말 로그아웃 하시겠습니까?',
-    messageFontSize: 'font-md',
+    message: '로그아웃 하시겠습니까?',
     btns: [
       { label: '확인', type: 'primary' },
       { label: '취소', type: 'secondary' },
@@ -23,13 +22,18 @@ export const modalProps: ModalPropsType = {
   },
   createNickname: {
     message: '닉네임을 입력해주세요.',
-    messageFontSize: 'font-md',
     btns: [{ label: '생성', type: 'primary' }],
+  },
+  gameResult: {
+    message: '',
+    messageFontSize: 'font-xl',
+    btns: [
+      { label: '재대결', type: 'primary' },
+      { label: '나가기', type: 'secondary' },
+    ],
   },
   error: {
     message: '',
-    messageFontSize: 'font-md',
     btns: [{ label: '확인', type: 'primary' }],
   },
 };
-
