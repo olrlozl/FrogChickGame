dad26677391bde6bda1c13320971e85dbcb7bed7
olrlozl <thakd221@naver.com>
2025-02-15 01:12:15 +0900

    feat: 친구 신청 api

--- backend/src/controllers/friend-controller.ts
+++ backend/src/controllers/friend-controller.ts
@@ -61,4 +61,51 @@ const searchFriend = async (
   }
 };
 
-export { searchFriend };
+const applyFriend = async (req: Request, res: Response, next: NextFunction) => {
+  const userId = req.userId as string;
+  const { to } = req.body;
+
+  try {
+    const user = await User.findById(userId).exec();
+    if (!user) {
+      return next(
+        new HttpError('사용자를 찾을 수 없습니다.', 401, 'INVALID_USERID')
+      );
+    }
+
+    const toUser = await User.findOne({ nickname: to }).exec();
+    if (!toUser) {
+      return next(
+        new HttpError('존재하지 않는 사용자입니다.', 404, 'UNKNOWN_USER')
+      );
+    }
+
+    if (user.friends.includes(toUser._id)) {
+      return next(new HttpError('이미 친구입니다.', 400, 'ALREADY_FRIEND'));
+    }
+
+    if (user.friendRequests.sent.includes(toUser._id)) {
+      return next(
+        new HttpError(
+          '이미 친구 신청을 보냈습니다.',
+          400,
+          'ALREADY_APPLY_FRIEND'
+        )
+      );
+    }
+
+    user.friendRequests.sent.push(toUser._id);
+    toUser.friendRequests.received.push(user._id);
+
+    await user.save();
+    await toUser.save();
+
+    res.status(200).send();
+  } catch (error) {
+    return next(
+      new HttpError('친구 신청에 실패했습니다.', 500, 'FAILED_APPLY_FRIEND')
+    );
+  }
+};
+
+export { searchFriend, applyFriend };
--- backend/src/routes/friend-route.ts
+++ backend/src/routes/friend-route.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { searchFriend } from '../controllers/friend-controller';
+import { searchFriend, applyFriend } from '../controllers/friend-controller';
 
 const friendRouter = Router();
 
 friendRouter.get('/search', searchFriend);
+friendRouter.post('/apply', applyFriend);
 
 export default friendRouter;
