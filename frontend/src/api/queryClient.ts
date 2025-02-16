0f0a78c6db52150856c774ebec2683051665563a
JangHyunsu <wkd940709@naver.com>
2025-02-16 12:08:25 +0900

    refactor: get요청과 그외 요청 네트워크 오류 분기

--- frontend/src/api/queryClient.ts
+++ frontend/src/api/queryClient.ts
@@ -1,5 +1,15 @@
-import { QueryClient } from '@tanstack/react-query';
+import { MutationCache, QueryClient } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+import { ERROR_MESSAGES } from 'constants/errorMessages';
+import { useErrorStore } from 'stores/errorStore';
 
-export const queryClient = new QueryClient(
-  // defaultOptions 추가 예정
-)
+export const queryClient = new QueryClient({
+  mutationCache: new MutationCache({
+    onError: (err) => {
+      const { setErrorMessage } = useErrorStore.getState();
+      if (err instanceof AxiosError && err.code === 'ERR_NETWORK') {
+        setErrorMessage(ERROR_MESSAGES.COMMON.ERR_NETWORK);
+      }
+    },
+  }),
+});
